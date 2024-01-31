787287678533463f80475bbdaf94cece8ab5b420
unknown <you@example.com>
2024-01-31 04:48:50 -0800

    firstcommit

---
+++ src/Country.jsx
@@ -0,0 +1,135 @@
+import {React, useState} from 'react'
+import './Country.css'
+import data from './rest-countries-api-with-color-theme-switcher-master (1)/rest-countries-api-with-color-theme-switcher-master/data.json'
+// import { click } from '@testing-library/user-event/dist/click';
+
+const Country = () => {
+
+  const [country, setCountry] = useState(data)
+  const [selectOption, setSelectOption] = useState('');
+  const [select, setSelect] = useState('');
+  const [currentPage,setCurrentPage] = useState(1);
+  const [countryPerPage,setCountryPerPage] = useState(20);
+  
+  const lastCountry = currentPage * countryPerPage
+  const firstCountry = lastCountry - countryPerPage
+
+  const res = country.slice(firstCountry,lastCountry)
+
+  const pageNumber = []
+  for (let i = 1; i <= Math.ceil(country.length/countryPerPage) ; i++) {
+    pageNumber.push(i)
+  }
+
+  const Continent = [
+    'Asia',
+    'Africa',
+    'Americas',
+    'Antartic Ocean',
+    'Europe',
+    'Oceania',
+    'Polar',
+  ];
+
+  const handleChange = (event)=>{
+    setSelectOption(event.target.value);
+  };
+
+  const foundRegion = country.filter((regions) => regions.region ===selectOption)
+
+  const handleChanged = (event) => {
+    setSelect(event.target.value);
+    const searchCountry = country.filter((region)=>{
+      const {name} = region;
+      return name.toLowerCase().includes(select.toLowerCase())
+    })
+    setCountry(searchCountry)
+  }
+
+  const handleNext = () => {
+    if (currentPage !== Math.ceil(country.length/countryPerPage)){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const handleBack = () => {
+    if (currentPage !== 1){
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  return (
+  
+    <div>
+       
+
+        <div className='nav'>
+        
+           <h6>Rest Countries API with Color Theme Switcher Master</h6>
+          <input type="text" placeholder='Search Region' onChange={(event) => handleChanged(event)} />
+
+          <select
+           name='Continent'
+           value={selectOption}
+           onChange={(event)=>handleChange(event)}>
+            <option>Continent</option>
+            {Continent.map((option) => (
+              <option key={option[0]}>{option}</option>
+            ))}
+        
+          </select>
+
+        </div>
+
+        <div  className='div1'>
+
+        { foundRegion.length > 0
+        ? foundRegion.map((country) =>{
+          return (
+            <div className='meet'>
+                  
+            <div className='div3'><img src={country.flags.png} alt="" /></div>
+            <h3>Country Name : {country.name}</h3>
+            
+              <p>Region : {country.region}</p>
+              <p>Population : {country.population}</p>
+              <p>Native Name : {country.nativeName}</p>       
+
+        </div>
+          );
+        })
+        
+       : res.map((country)=>{
+        return (
+          <div className='meet' key={country.name}>
+                  
+          <div className='div3'><img src={country.flags.png} alt="" /></div>
+          <h3>Country Name : {country.name}</h3>
+          <p>Region : {country.region}</p>
+          <p>Population : {country.population}</p>
+          <p>Native Name : {country.nativeName}</p>
+
+      </div>
+        );
+       })}
+
+        </div>
+        
+        <div className='divfeet'>
+
+          <footer>
+
+              <button onClick={()=>handleBack()}>Previous</button>
+              {pageNumber.map((page)=>{
+                return <b onClick={()=>setCurrentPage(page)} style={{cursor:'pointer'}}>{page}</b>
+              })}
+              <button onClick={()=>handleNext()}>Next</button>
+
+          </footer>
+
+        </div>
+    </div>
+  );
+};
+
+export default Country
